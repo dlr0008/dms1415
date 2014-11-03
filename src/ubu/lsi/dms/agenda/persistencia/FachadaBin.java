0489d4e9be15557f7c74be32860ee7a4d0da58e9
dlr0008 <dlr0008@alu.ubu.es>
2014-11-03 18:38:50 +0100

    Version 1.0 de Fachada Persistente e hijos

--- src/ubu/lsi/dms/agenda/persistencia/FachadaBin.java
+++ src/ubu/lsi/dms/agenda/persistencia/FachadaBin.java
@@ -3,10 +3,70 @@
  */
 package ubu.lsi.dms.agenda.persistencia;
 
+import java.util.Collection;
+
+import ubu.lsi.dms.agenda.modelo.Contacto;
+import ubu.lsi.dms.agenda.modelo.Llamada;
+import ubu.lsi.dms.agenda.modelo.TipoContacto;
+
 /**
  * @author alumno
  *
  */
 public class FachadaBin implements FachadaPersistente {
+
+	@Override
+	public Contacto getContacto(String apellido) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Llamada> getLlamadas(Contacto contacto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TipoContacto getTipoContacto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateContacto(Contacto contacto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateLlamada(Llamada llamada) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTipoContacto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertContacto(Contacto contacto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertLlamada(Llamada llamada) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertTipoContacto(int idTipoContacto, String TipoContacto) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/ubu/lsi/dms/agenda/persistencia/FachadaPersistente.java
+++ src/ubu/lsi/dms/agenda/persistencia/FachadaPersistente.java
@@ -1,12 +1,38 @@
-/**
- * 
- */
 package ubu.lsi.dms.agenda.persistencia;
 
+import java.util.Collection;
+import ubu.lsi.dms.agenda.modelo.Contacto;
+import ubu.lsi.dms.agenda.modelo.Llamada;
+import ubu.lsi.dms.agenda.modelo.TipoContacto;
+
 /**
- * @author alumno
- *
+ * Interfaz de FachadaPersistente
+ * 
+ * @author <A HREF="mailto:jld0016@alu.ubu.es">Jorge Laguna</A>
+ * @author <A HREF="mailto:rmp0046@alu.ubu.es">Roberto Miranda</A>
+ * @author <A HREF="mailto:aam0093@alu.ubu.es">Asier Alonso</A>
+ * @author <A HREF="mailto:dlr0008@alu.ubu.es">Daniel Lozano</A> 
+ * @version 1.0
+ * 
  */
 public interface FachadaPersistente {
+
+	public Contacto getContacto(String apellido);
+
+	public Collection<Llamada> getLlamadas(Contacto contacto);
+
+	public TipoContacto getTipoContacto();
+
+	public void updateContacto(Contacto contacto);
+
+	public void updateLlamada(Llamada llamada);
+
+	public void updateTipoContacto();
+	
+	public void insertContacto(Contacto contacto);
+	
+	public void insertLlamada(Llamada llamada);
+	
+	public void insertTipoContacto(int idTipoContacto, String TipoContacto);
 
 }
