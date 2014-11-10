dce5833c02f70e05b6e1b0f919417cca85c28e61
Roberto <Roberto@Rober_Portatil>
2014-11-10 23:46:33 +0100

    Resueltos varios errores

--- src/ubu/lsi/dms/agenda/persistencia/FachadaBin.java
+++ src/ubu/lsi/dms/agenda/persistencia/FachadaBin.java
@@ -5,14 +5,15 @@ package ubu.lsi.dms.agenda.persistencia;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.Collection;
 
-import sun.security.jca.GetInstance;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import ubu.lsi.dms.agenda.modelo.Contacto;
 import ubu.lsi.dms.agenda.modelo.Llamada;
 import ubu.lsi.dms.agenda.modelo.TipoContacto;
@@ -31,104 +32,110 @@ public class FachadaBin implements FachadaPersistente {
 			+ File.separator + "tipos.dat";
 	private static final FachadaPersistente fachadaBin = new FachadaBin();
 
-	private static ObjectOutputStream outContactos = null;
-	private static ObjectInputStream inContactos = null;
-	private static ObjectOutputStream outLlamadas = null;
-	private static ObjectInputStream inLlamadas = null;
-
 	public FachadaBin() {
 	}
 
 	public static FachadaPersistente getInstance() {
-		try {
-			outContactos = new ObjectOutputStream(new FileOutputStream(
-					fileContactos.toString(), true));
-			inContactos = new ObjectInputStream(new FileInputStream(
-					fileContactos.toString()));
-			outLlamadas = new ObjectOutputStream(new FileOutputStream(
-					fileLlamadas.toString(), true));
-			inLlamadas = new ObjectInputStream(new FileInputStream(
-					fileLlamadas.toString()));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		return fachadaBin;
 
 	}
 
 	@Override
 	public Collection<Contacto> getContacto(String apellido) {
-		Collection<Contacto> contactosFiltrado = null;
-
+		List<Contacto> contactosFiltrado = new ArrayList<Contacto>();
+		List<Contacto> contactos = new ArrayList<Contacto>();
+		ObjectInputStream in = null;
 		try {
-			while (true) {
-
-				Contacto c = (Contacto) inContactos.readObject();
-				if (c.getApellidos().equals(apellido)) {
+			in = new ObjectInputStream(new FileInputStream(fileContactos));
+			contactos = (ArrayList<Contacto>) in.readObject();
+			for (Contacto c : contactos) {
+				System.out.println(c.toString());
+				if (c.getApellidos().equals(apellido))
 					contactosFiltrado.add(c);
-				}
 
 			}
 
 		} catch (IOException | ClassNotFoundException E) {
-			System.err.print("No se pudo obtener el contacto");
-			inContactos.close();
+			E.printStackTrace();
 		} finally {
-
-			return contactosFiltrado;
+			try {
+				in.close();
+				return contactosFiltrado;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+		return contactosFiltrado;
 	}
 
 	@Override
 	public Collection<Llamada> getLlamadas(Contacto contacto) {
-		Collection<Llamada> llamadasFiltrado = null;
-		try {
-			while (true) {
 
-				Llamada l = (Llamada) inLlamadas.readObject();
-				if ((l.getContacto().getNombre().equals(contacto.getNombre()))) {
-					System.out.println(l.toString());
+		List<Llamada> llamadasFiltrado = new ArrayList<Llamada>();
+		List<Llamada> llamadas = new ArrayList<Llamada>();
+		ObjectInputStream in = null;
+		try {
+			in = new ObjectInputStream(new FileInputStream(fileLlamadas));
+			llamadas = (ArrayList<Llamada>) in.readObject();
+			System.out.println(llamadas.size());
+			for (Llamada l : llamadas) {
+				System.out.println(l.toString());
+				if (l.getContacto().equals(contacto))
 					llamadasFiltrado.add(l);
-				}
 
 			}
-		} catch (IOException | ClassNotFoundException E) {
-			inLlamadas.close();
 		} finally {
-			
+			if (llamadasFiltrado == null)
+				System.err.println("No existen llamadas del contacto "
+						+ contacto.getNombre());
 			return llamadasFiltrado;
 		}
 	}
 
 	@Override
 	public Collection<TipoContacto> getTipoContacto() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void insertContacto(Contacto contacto) {
-
+		ObjectOutputStream out = null;
 		try {
-			outContactos.writeObject(contacto);
+			out = new ObjectOutputStream(new FileOutputStream(fileContactos,
+					true));
+			out.writeObject(contacto);
+
 		} catch (IOException E) {
 			System.err.println("No se pudo insertar el contacto");
 
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
 
 	@Override
 	public void insertLlamada(Llamada llamada) {
+		ObjectOutputStream out = null;
 		try {
-			outLlamadas.writeObject(llamada);
-		} catch (IOException E) {
-			System.err.println("LLamada");
+			out = new ObjectOutputStream(new FileOutputStream(fileLlamadas,
+					true));
+			out.writeObject(llamada);
 
+		} catch (IOException E) {
+			System.err.println("No se pudo insertar el contacto");
+
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+	
+				e.printStackTrace();
+			}
 		}
 
 	}
--- src/ubu/lsi/dms/agenda/test/initfiles.java
+++ src/ubu/lsi/dms/agenda/test/initfiles.java
@@ -1,44 +1,22 @@
 package ubu.lsi.dms.agenda.test;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.logging.Logger;
 
-import ubu.lsi.dms.agenda.modelo.*;
+import ubu.lsi.dms.agenda.modelo.Contacto;
+import ubu.lsi.dms.agenda.modelo.Llamada;
+import ubu.lsi.dms.agenda.modelo.TipoContacto;
 import ubu.lsi.dms.agenda.persistencia.FabricaBin;
-import ubu.lsi.dms.agenda.persistencia.FachadaBin;
 import ubu.lsi.dms.agenda.persistencia.FachadaPersistente;
 
 public class initfiles {
-
-	public static void main(String[] args) throws IOException,
-			ClassNotFoundException {
-		String fileContactos = "." + File.separator + "res" + File.separator
-				+ "contactos.dat";
-		String filellamadas = "." + File.separator + "res" + File.separator
-				+ "contactos.dat";
-		Collection<Contacto> contactos = new ArrayList<Contacto>();
-		iniContactos(contactos);
-		FabricaBin fabricabinaria = new FabricaBin();
-		FachadaPersistente fachadabinaria = fabricabinaria
-				.createFachadaPersistente();
-		for (Contacto c : contactos) {
-			System.out.println(c.toString());
-			fachadabinaria.insertContacto(c);
-		}
-		System.out.println("FINALIZADA INSERCIÓN");
-		
-		testContactos(contactos,fachadabinaria);
-
-	}
 
 	private static void iniContactos(Collection<Contacto> contactos) {
 		TipoContacto tipoContacto = new TipoContacto(1, "Tipo001");
 		for (int i = 1; i <= 9; i++)
 			contactos.add(new Contacto(i, "Nombre00" + i, "Apellidos00" + i,
-					"Estimado00" + i, " Direccion00" + i, "Ciudad00" + i,
+					"Estimado00" + i, "Direccion00" + i, "Ciudad00" + i,
 					"Prov00" + i, "CodPostal000" + i, "Region000" + i,
 					"Pais000" + i, "NombreCompania000" + i, "Cargo000" + i,
 					"TelefonoTrabajo00" + i, "ExtensionTrabajo00" + i,
@@ -48,16 +26,90 @@ public class initfiles {
 
 	}
 
-	private static void testContactos(Collection<Contacto> contactos,
-			FachadaPersistente fachadabinaria) {
-
+	private static void iniLlamadas(Collection<Llamada> llamadas,
+			Collection<Contacto> contactos) {
 		int i = 1;
-
 		for (Contacto c : contactos) {
-
-			assert c.equals(fachadabinaria.getContacto("Apellido00" + i));
+			llamadas.add(new Llamada(i, c, "2014-10-18 0" + i + ":00:00",
+					"AsuntoLlamada00" + i, "NotaLlamada00" + i));
 			i++;
 		}
+	}
 
+	public static void main(String[] args) throws IOException,
+			ClassNotFoundException {
+		Collection<Contacto> contactos = new ArrayList<Contacto>();
+		Collection<Llamada> llamadas = new ArrayList<Llamada>();
+		iniContactos(contactos);
+		iniLlamadas(llamadas, contactos);
+		FabricaBin fabricabinaria = new FabricaBin();
+		FachadaPersistente fachadabinaria = fabricabinaria
+				.createFachadaPersistente();
+		for (Contacto c : contactos) {
+			// System.out.println(c.toString());
+			fachadabinaria.insertContacto(c);
+		}
+		for (Llamada l : llamadas) {
+			// System.out.println(l.toString());
+			fachadabinaria.insertLlamada(l);
+		}
+		System.out.println("FINALIZADA INSERCIÓN");
+
+		testContactos(contactos, fachadabinaria);
+		// testLlamadas(contactos, fachadabinaria);
+
+		System.out.println("OK");
+
+	}
+
+	private static void testContactos(Collection<Contacto> contactos,
+			FachadaPersistente fachadabinaria) {
+		Collection<Contacto> todos = new ArrayList<Contacto>();
+		for (int i = 1; i <= 9; i++) {
+			todos = fachadabinaria.getContacto("Apellidos00" + i);
+			assert (todos.size() == 1);
+			for (Contacto c : todos) {
+				assert ("Nombre00" + i).equals(c.getNombre());
+				assert ("Apellidos00" + i).equals(c.getApellidos());
+				assert ("Estimado00" + i).equals(c.getEstimado());
+				assert ("Direccion00" + i).equals(c.getDireccion());
+				assert ("Ciudad00" + i).equals(c.getCiudad());
+				assert ("Prov00" + i).equals(c.getProv());
+				assert ("CodPostal000" + i).equals(c.getCodPostal());
+				assert ("Region000" + i).equals(c.getRegion());
+				assert ("TelefonoTrabajo00" + i).equals(c.getTelefonoTrabajo());
+				assert ("ExtensionTrabajo00" + i).equals(c
+						.getExtensionTrabajo());
+				assert ("TelefonoMovil00" + i).equals(c.getTelefonoMovil());
+				assert ("TelefonoTrabajo00" + i).equals(c.getTelefonoTrabajo());
+				assert (" NomCorreoElectronico00" + i + "@ubu.es").equals(c
+						.getNomCorreoElectronico());
+				assert ("Notas00" + i).equals(c.getNotas());
+			}
+		}
+
+	}
+
+	private static void testLlamadas(Collection<Contacto> contactos,
+			FachadaPersistente fachadabinaria) {
+		Collection<Llamada> llamadasTest = null;
+		Collection<Contacto> contacto = new ArrayList<Contacto>();
+		Collection<Llamada> todos = new ArrayList<Llamada>();
+		int i = 1;
+		contacto = fachadabinaria.getContacto("Apellidos001");
+		for (Contacto c : contacto) {
+			todos = fachadabinaria.getLlamadas(c);
+			for (Llamada l : todos) {
+				// assert i.equals(l.getIdLlamada();
+				assert ("Nombre001".equals(l.getContacto().getNombre()));
+				assert ("2014-10-18 0" + i + ":00:00").equals(l
+						.getFechaLlamada());
+				assert ("2014-10-18 0" + i + ":00:00").equals(l
+						.getFechaLlamada());
+				assert ("AsuntoLlamada00" + i).equals(l.getAsunto());
+				i++;
+
+			}
+		}
 	}
 }
