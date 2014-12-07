4998af12f07fbd2dc7682ac72eded121d5698472
rober920 <rmp0046@gmail.com>
2014-12-07 13:16:26 +0100

    Panel para añadir nuevos tipos de Contacto, aun sin ser utilizable

---
+++ src/ubu/lsi/dms/agenda/gui/JPanelNuevoTipo.java
@@ -0,0 +1,27 @@
+package ubu.lsi.dms.agenda.gui;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+public class JPanelNuevoTipo extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public JPanelNuevoTipo() {
+		setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(126, 99, 89, 23);
+		add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(279, 226, 89, 23);
+		add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(231, 47, 89, 23);
+		add(btnNewButton_2);
+
+	}
+}
