8abf79b597794e34e4d65632d0f1c6cd0db34486
rober920 <rmp0046@gmail.com>
2014-12-07 13:13:26 +0100

    Añadidas nuevas funcionalidades al Jpanel de Consulta. Ahora busca correctamente

--- src/ubu/lsi/dms/agenda/gui/JPanelConsulta.java
+++ src/ubu/lsi/dms/agenda/gui/JPanelConsulta.java
@@ -5,16 +5,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.util.ArrayList;
-import java.util.Collection;
 
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.JButton;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableColumnModel;
@@ -23,45 +22,32 @@ import javax.swing.table.TableColumn;
 import ubu.lsi.dms.agenda.gui.tables.TablaContactos;
 import ubu.lsi.dms.agenda.gui.tables.TablaLlamadas;
 import ubu.lsi.dms.agenda.gui.tables.TablaTipos;
-import ubu.lsi.dms.agenda.modelo.Contacto;
-import ubu.lsi.dms.agenda.modelo.Llamada;
-import ubu.lsi.dms.agenda.modelo.TipoContacto;
+import ubu.lsi.dms.agenda.test.TestGui;
+import javax.swing.JLabel;
 
 public class JPanelConsulta extends JPanel {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
 	/**
 	 * Create the panel.
 	 */
 	private ButtonGroup botones = new ButtonGroup();
 	private JRadioButton rdbtnContactos = new JRadioButton("Contactos");
 	private JRadioButton rdbtnLlamadas = new JRadioButton("Llamadas");
+	private JButton btnMostrar = new JButton("Mostrar");
+
 	private JRadioButton rdbtnTiposDeContacto = new JRadioButton(
 			"Tipos de Contacto");
 	private JTextField campo = new JTextField();
+	private JLabel lblIntroduce = new JLabel("Introduce el apellido a consultar");
 	private JScrollPane listScroller = null;
+	private TestGui testGui = null;
 
-	private Collection<Contacto> contactos = new ArrayList<Contacto>();
-	private Collection<Llamada> llamadas = new ArrayList<Llamada>();
-	private Collection<TipoContacto> tipos = new ArrayList<TipoContacto>();
-	private JTable tabla = null;
+	public JPanelConsulta(TestGui testGui) {
 
-	private String[] cabeceraContacto = new String[] { "Id", "Nombre",
-			"Apellidos", "Estimado", "Direccion", "Ciudad", "Prov",
-			"CodPostal", "Region", "Páis", "NombreCompañia", "Cargo",
-			"telefonoTrabajo", "ExtensionTrabajo", "TelefonoMovil", "NumFax",
-			"NomCorreoElectronico", "Notas" };
-	private String[] cabeceraLlamada = new String[] { "ID", "Contacto",
-			"Fecha", "Asunto", "Notas" };
-	private String[] cabeceraTipos = new String[] { "ID", "Nombre" };
-
-	public JPanelConsulta() {
-		iniContactos(contactos);
-		iniLlamadas(llamadas);
-		iniTipos(tipos);
+		this.testGui = testGui;
 
 		rdbtnContactos.setBounds(6, 5, 113, 23);
 		rdbtnContactos.addItemListener(new rbttonEnableField());
@@ -87,22 +73,77 @@ public class JPanelConsulta extends JPanel {
 		add(campo);
 		campo.setColumns(10);
 
-		JButton btnConsultar = new JButton("Consultar");
-		btnConsultar.setBounds(400, 31, 107, 23);
-		btnConsultar.addActionListener(new ButtonEnable());
-		add(btnConsultar);
+		btnMostrar.setBounds(400, 31, 107, 23);
+		btnMostrar.addActionListener(new buttonConsulta());
+		add(btnMostrar);
 
-		TablaContactos contactosEnTabla = new TablaContactos(contactos);
+		TablaContactos contactosEnTabla = new TablaContactos(
+				testGui.getContactos());
 
-		JTable tabla = crearTabla(contactosEnTabla, cabeceraContacto);
+		JTable tabla = crearTabla(contactosEnTabla,
+				contactosEnTabla.getCabecera());
 
 		crearListaConsultas(tabla);
 
+		JButton btnNewButton = new JButton("Mostrar Todos");
+		btnNewButton.addActionListener(new buttonTodos());
+		btnNewButton.setBounds(517, 31, 126, 23);
+		add(btnNewButton);
+		
+		
+		lblIntroduce.setBounds(179, 9, 211, 14);
+		add(lblIntroduce);
+
 	}
 
-	private void iniTipos(Collection<TipoContacto> tipos) {
-		for (int i = 1; i <= 30; i++)
-			tipos.add(new TipoContacto(i, "Tipo00" + i));
+	private class buttonConsulta implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			AbstractTableModel modeloTabla = null;
+			String[] cabecera = null;
+			if (campo.getText().equals("") && campo.isEnabled()) {
+				JOptionPane.showMessageDialog(null,
+						"Introduce un criterio de Busqueda");
+			} else {
+				if (rdbtnLlamadas.isSelected()) {
+					modeloTabla = new TablaLlamadas(
+							testGui.filtrarLLamadas(campo.getText()));
+					cabecera = ((TablaLlamadas) modeloTabla).getCabecera();
+				}
+				if (rdbtnContactos.isSelected()) {
+					modeloTabla = new TablaContactos(
+							testGui.filtrarContactos(campo.getText()));
+					cabecera = ((TablaContactos) modeloTabla).getCabecera();
+				}
+
+				crearListaConsultas(crearTabla(modeloTabla, cabecera));
+			}
+		}
+	}
+
+	private class buttonTodos implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			AbstractTableModel modeloTabla = null;
+			String[] cabecera = null;
+			if (rdbtnLlamadas.isSelected()) {
+				modeloTabla = new TablaLlamadas(testGui.getLlamadas());
+				cabecera = ((TablaLlamadas) modeloTabla).getCabecera();
+			}
+			if (rdbtnContactos.isSelected()) {
+				modeloTabla = new TablaContactos(testGui.getContactos());
+				cabecera = ((TablaContactos) modeloTabla).getCabecera();
+			}
+			if (rdbtnTiposDeContacto.isSelected()) {
+				modeloTabla = new TablaTipos(testGui.getTipos());
+				cabecera = ((TablaTipos) modeloTabla).getCabecera();
+			}
+
+			crearListaConsultas(crearTabla(modeloTabla, cabecera));
+
+		}
 
 	}
 
@@ -113,6 +154,8 @@ public class JPanelConsulta extends JPanel {
 			if (e.getStateChange() == ItemEvent.SELECTED) {
 				campo.setEnabled(false);
 				campo.setBackground(JPanelConsulta.this.getBackground());
+				btnMostrar.setEnabled(false);
+				lblIntroduce.setEnabled(false);
 			}
 
 		}
@@ -125,30 +168,29 @@ public class JPanelConsulta extends JPanel {
 			if (e.getStateChange() == ItemEvent.SELECTED) {
 				campo.setEnabled(true);
 				campo.setBackground(Color.WHITE);
+				btnMostrar.setEnabled(true);
+				lblIntroduce.setEnabled(true);
 			}
 
 		}
 	}
 
-	private class ButtonEnable implements ActionListener {
+	private void crearListaConsultas(JTable tabla) {
+		if (listScroller != null)
+			remove(listScroller);
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			if (rdbtnLlamadas.isSelected()) {
-				TablaLlamadas llamadasEnTabla = new TablaLlamadas(llamadas);
-				tabla = crearTabla(llamadasEnTabla, cabeceraLlamada);
-			}
-			if (rdbtnContactos.isSelected()) {
-				TablaContactos contactosEnTabla = new TablaContactos(contactos);
-				tabla = crearTabla(contactosEnTabla, cabeceraContacto);
-			}
-			if (rdbtnTiposDeContacto.isSelected()) {
-				TablaTipos tiposEnTabla = new TablaTipos(tipos);
-				tabla = crearTabla(tiposEnTabla, cabeceraTipos);
-			}
-			crearListaConsultas(tabla);
+		listScroller = new JScrollPane(tabla);
+		listScroller.setBounds(6, 87, 929, 467);
+		if (tabla.getRowCount() == 0)
+			JOptionPane.showMessageDialog(null, "Lista Vacia");
 
-		}
+		listScroller
+				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		listScroller
+				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		add(listScroller);
+		validate();
+		repaint();
 	}
 
 	private JTable crearTabla(AbstractTableModel datos, String[] cabecera) {
@@ -169,51 +211,5 @@ public class JPanelConsulta extends JPanel {
 			table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 		table.setRowHeight(20);
 		return table;
-	}
-
-	private void iniContactos(Collection<Contacto> contactos) {
-		TipoContacto tipoContacto = new TipoContacto(1, "Tipo001");
-		for (int i = 1; i <= 40; i++)
-			contactos.add(new Contacto(i, "Nombre00" + i, "Apellidos00" + i,
-					"Estimado00" + i, "Direccion00" + i, "Ciudad00" + i,
-					"Prov00" + i, "CodPostal000" + i, "Region000" + i,
-					"Pais000" + i, "NombreCompania000" + i, "Cargo000" + i,
-					"TelefonoTrabajo00" + i, "ExtensionTrabajo00" + i,
-					"TelefonoMovil00" + i, "NumFax00" + i,
-					" NomCorreoElectronico00" + i + "@ubu.es", "Notas00" + i,
-					tipoContacto));
-
-	}
-
-	private void iniLlamadas(Collection<Llamada> llamadas) {
-		TipoContacto tipoContacto = new TipoContacto(1, "Tipo001");
-
-		for (int i = 1; i <= 5; i++) {
-			llamadas.add(new Llamada(i, new Contacto(1, "Nombre00" + 1,
-					"Apellidos00" + 1, "Estimado00" + 1, "Direccion00" + 1,
-					"Ciudad00" + 1, "Prov00" + 1, "CodPostal000" + i,
-					"Region000" + i, "Pais000" + 1, "NombreCompania000" + 1,
-					"Cargo000" + 1, "TelefonoTrabajo00" + 1,
-					"ExtensionTrabajo00" + 1, "TelefonoMovil00" + 1,
-					"NumFax00" + 1, " NomCorreoElectronico00" + 1 + "@ubu.es",
-					"Notas00" + 1, tipoContacto),
-					"2014-10-18 0" + i + ":00:00", "AsuntoLlamada00" + i,
-					"NotaLlamada00" + i));
-		}
-	}
-
-	private void crearListaConsultas(JTable tabla) {
-		if (listScroller != null)
-			remove(listScroller);
-
-		listScroller = new JScrollPane(tabla);
-		listScroller.setBounds(6, 87, 929, 467);
-		listScroller
-				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		listScroller
-				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-		add(listScroller);
-		validate();
-		repaint();
 	}
 }
