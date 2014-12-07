dfc01e59bc600d382b2905c0acc06358e5b94a60
rober920 <rmp0046@gmail.com>
2014-12-07 13:12:42 +0100

    Nueva version del Menu, que cambia el JPanel de la ventana principal.

--- src/ubu/lsi/dms/agenda/gui/JMenuAgenda.java
+++ src/ubu/lsi/dms/agenda/gui/JMenuAgenda.java
@@ -1,48 +1,90 @@
 package ubu.lsi.dms.agenda.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JToolBar;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+import ubu.lsi.dms.agenda.test.TestGui;
+
 public class JMenuAgenda extends JPanel {
-	public JMenuAgenda() {
+
+	private JFramePrincipal frame = null;
+	private TestGui testGui = null;
+
+	private static final long serialVersionUID = 1L;
+
+	public JMenuAgenda(JFramePrincipal jFramePrincipal, TestGui testGui) {
 		setLayout(null);
-		
+		frame = jFramePrincipal;
+		this.testGui = testGui;
+
 		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBounds(0, 0, 256, 50);
+		menuBar.setBounds(0, 0, 950, 25);
 		add(menuBar);
-		
+
 		javax.swing.JMenu mnInsertar = new javax.swing.JMenu("Insertar");
 		menuBar.add(mnInsertar);
-		
+
 		JMenuItem mntmNewMenuItem = new JMenuItem("Nuevo Contacto");
 		mnInsertar.add(mntmNewMenuItem);
-		
+
 		JMenuItem menuItem = new JMenuItem("Nueva Llamada");
 		mnInsertar.add(menuItem);
-		
-		JMenuItem mntmNuevoTipocontacto = new JMenuItem("Nuevo Tipo de Contacto");
+
+		JMenuItem mntmNuevoTipocontacto = new JMenuItem(
+				"Nuevo Tipo de Contacto");
+		mntmNuevoTipocontacto.addActionListener(new menuChange(
+				new JPanelNuevoTipo()));
 		mnInsertar.add(mntmNuevoTipocontacto);
-		
+
 		javax.swing.JMenu mnModificar = new javax.swing.JMenu("Modificar");
 		menuBar.add(mnModificar);
-		
+
 		JMenuItem mntmContacto = new JMenuItem("Contacto");
 		mnModificar.add(mntmContacto);
-		
+
 		JMenuItem mntmLlamada = new JMenuItem("Llamada");
 		mnModificar.add(mntmLlamada);
-		
+
 		JMenuItem mntmTipoDeContacto = new JMenuItem("Tipo de Contacto");
 		mnModificar.add(mntmTipoDeContacto);
-		
+
 		javax.swing.JMenu mnConsultar = new javax.swing.JMenu("Consultar");
 		menuBar.add(mnConsultar);
-		
+
+		JMenuItem mntmNuevaConsulta = new JMenuItem("Nueva Consulta");
+		mntmNuevaConsulta.addActionListener(new menuChange(new JPanelConsulta(
+				testGui)));
+		mnConsultar.add(mntmNuevaConsulta);
+
 		javax.swing.JMenu mnAyuda = new javax.swing.JMenu("Ayuda");
 		menuBar.add(mnAyuda);
 	}
+
+	private class menuChange implements ActionListener {
+		JPanel panel = null;
+
+		public menuChange(JPanel panel) {
+			this.panel = panel;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			JPanel viejoPanel = frame.getPanel();
+			System.out.println(viejoPanel);
+			panel.setBounds(viejoPanel.getX(), viejoPanel.getY(),
+					viejoPanel.getWidth(), viejoPanel.getHeight());
+			viejoPanel.removeAll();
+			frame.remove(viejoPanel);
+			frame.setPanel(panel);
+			frame.add(panel);
+			frame.validate();
+			frame.repaint();
+		}
+	}
 }
-
-
